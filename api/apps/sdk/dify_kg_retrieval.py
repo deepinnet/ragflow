4d43689b4b9d7cffc81d8bac2042231297282b1f
kinglen <kinglen.maoxy@gmail.com>
2025-04-08 14:45:18 +0800

    添加支持知识图谱搜索的接口

---
+++ api/apps/sdk/dify_kg_retrieval.py
@@ -0,0 +1,29 @@
+#
+#  Copyright 2024 The InfiniFlow Authors. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+from flask import request
+
+from api.utils.api_utils import validate_request, apikey_required
+import logging
+from .dify_retrieval import retrieval
+
+@manager.route('/dify/kg/retrieval', methods=['POST'])  # noqa: F821
+@apikey_required
+@validate_request("knowledge_id", "query")
+def kg_retrieval(tenant_id):
+    req = request.json
+    req["use_kg"] = True  # 设置use_kg为True
+    logging.info(f"Using knowledge graph retrieval mode, request params: {req}")  # 添加日志，包含请求参数
+    return retrieval(tenant_id)
